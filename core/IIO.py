923a30f0c45b16548b5b3e32a614b0b3c60ebb87
SHPI <46942752+shpi@users.noreply.github.com>
2025-07-03 13:41:50 +0200

    enhance IIO properties

--- core/IIO.py
+++ core/IIO.py
@@ -10,6 +10,52 @@ import core.iio as iio
 from core.DataTypes import Convert, DataType
 from core.Property import EntityProperty
 
+# Map common attribute names to their DataType and a short description.
+ATTRIBUTE_INFO = {
+    'raw': (DataType.INT, 'Unscaled raw value'),
+    'input': (DataType.INT, 'Processed input value'),
+    'scale': (DataType.FLOAT, 'Scale factor applied to raw value'),
+    'offset': (DataType.FLOAT, 'Offset applied to raw value'),
+    'calibbias': (DataType.FLOAT, 'Calibration bias value'),
+    'calibscale': (DataType.FLOAT, 'Calibration scale value'),
+    'sampling_frequency': (DataType.FLOAT, 'Sampling frequency in Hz'),
+    'integration_time': (DataType.FLOAT, 'Integration time'),
+    'oversampling_ratio': (DataType.INT, 'Oversampling ratio'),
+    'hysteresis': (DataType.FLOAT, 'Hysteresis value'),
+    'filter_low_pass_0_frequency': (DataType.FLOAT, 'Low pass filter frequency'),
+    'filter_high_pass_0_frequency': (DataType.FLOAT, 'High pass filter frequency'),
+    'enable': (DataType.BOOL, 'Enable or disable the channel'),
+}
+
+# Descriptive text for known IIO channel types.
+CHANNEL_DESCRIPTIONS = {
+    iio.ChannelType.IIO_VOLTAGE: 'Voltage measurement',
+    iio.ChannelType.IIO_CURRENT: 'Current measurement',
+    iio.ChannelType.IIO_POWER: 'Power measurement',
+    iio.ChannelType.IIO_ACCEL: 'Acceleration measurement',
+    iio.ChannelType.IIO_MAGN: 'Magnetic field measurement',
+    iio.ChannelType.IIO_LIGHT: 'Light intensity measurement',
+    iio.ChannelType.IIO_INTENSITY: 'Light intensity measurement',
+    iio.ChannelType.IIO_PROXIMITY: 'Proximity measurement',
+    iio.ChannelType.IIO_TEMP: 'Temperature measurement',
+    iio.ChannelType.IIO_ROT: 'Angular velocity measurement',
+    iio.ChannelType.IIO_ANGL: 'Angular measurement',
+    iio.ChannelType.IIO_TIMESTAMP: 'Timestamp',
+    iio.ChannelType.IIO_CAPACITANCE: 'Capacitance measurement',
+    iio.ChannelType.IIO_PRESSURE: 'Pressure measurement',
+    iio.ChannelType.IIO_HUMIDITYRELATIVE: 'Relative humidity measurement',
+    iio.ChannelType.IIO_STEPS: 'Step count',
+    iio.ChannelType.IIO_ENERGY: 'Energy measurement',
+    iio.ChannelType.IIO_DISTANCE: 'Distance measurement',
+    iio.ChannelType.IIO_VELOCITY: 'Velocity measurement',
+    iio.ChannelType.IIO_CONCENTRATION: 'Concentration measurement',
+    iio.ChannelType.IIO_RESISTANCE: 'Electrical resistance measurement',
+    iio.ChannelType.IIO_PH: 'pH level',
+    iio.ChannelType.IIO_UVINDEX: 'UV index',
+    iio.ChannelType.IIO_ELECTRICALCONDUCTIVITY: 'Electrical conductivity',
+    iio.ChannelType.IIO_COUNT: 'Count value',
+    iio.ChannelType.IIO_GRAVITY: 'Gravity measurement',
+}
 
 class IIO:
     """Class for retrieving the requested information."""
@@ -126,12 +172,13 @@ class IIO:
                 offset = 0
                 raw = None
 
+                channel_desc = CHANNEL_DESCRIPTIONS.get(channel.type, channel.id)
                 self.properties[f'{dev.name}/{channel.id}'] = EntityProperty(
-                                                                             category='sensor/'+dev.name,
-                                                                             name=channel.id,
-                                                                             description=dev.name + ' ' + channel.id,
-                                                                             type=Convert.iio_to_shpi(channel.type),
-                                                                             interval=20)
+                    category='sensor/' + dev.name,
+                    name=channel.id,
+                    description=f"{dev.name} {channel_desc}",
+                    type=Convert.iio_to_shpi(channel.type),
+                    interval=20)
 
                 for channel_attr in channel.attrs:
                     path = channel.attrs[channel_attr].filename
@@ -154,30 +201,34 @@ class IIO:
                         raw = channel.attrs[channel_attr].value
 
                     elif channel_attr.endswith('_available'):
-                        if f'{dev.name}/{channel.id}/{channel_attr[:-10]}' not in self.properties:
-                            self.properties[f'{dev.name}/{channel.id}/{channel_attr[:-10]}'] = EntityProperty(
-
+                        base = channel_attr[:-10]
+                        dtype, adesc = ATTRIBUTE_INFO.get(base, (DataType.FLOAT, base))
+                        if f'{dev.name}/{channel.id}/{base}' not in self.properties:
+                            self.properties[f'{dev.name}/{channel.id}/{base}'] = EntityProperty(
                                 category='sensor/' + dev.name,
-                                name=channel_attr[:-10],
-                                description=dev.name + ' ' + channel.id,
-                                type=DataType.FLOAT,
+                                name=base,
+                                description=f"{dev.name} {channel.id} {adesc}",
+                                type=dtype,
                                 available=channel.attrs[channel_attr].value.split(),
                                 interval=-1)
                         else:
-                            self.properties[f'{dev.name}/{channel.id}/{channel_attr[:-10]}'].available = channel.attrs[channel_attr].value.split()
+                            self.properties[f'{dev.name}/{channel.id}/{base}'].available = channel.attrs[channel_attr].value.split()
 
                     else:
                         if f'{dev.name}/{channel.id}/{channel_attr}' not in self.properties:
+                            dtype, adesc = ATTRIBUTE_INFO.get(channel_attr, (DataType.FLOAT, channel_attr))
                             self.properties[f'{dev.name}/{channel.id}/{channel_attr}'] = EntityProperty(
-
                                 category='sensor/' + dev.name,
                                 name=channel_attr,
-                                type=DataType.FLOAT,
+                                description=f"{dev.name} {channel.id} {adesc}",
+                                type=dtype,
                                 interval=-1)
 
-                        self.properties[f'{dev.name}/{channel.id}/{channel_attr}'].description = dev.name + ' ' + channel.id + ' ' + channel_attr
+                        dtype, adesc = ATTRIBUTE_INFO.get(channel_attr, (DataType.FLOAT, channel_attr))
+                        self.properties[f'{dev.name}/{channel.id}/{channel_attr}'].description = f"{dev.name} {channel.id} {adesc}"
                         self.properties[f'{dev.name}/{channel.id}/{channel_attr}'].name = channel_attr
                         self.properties[f'{dev.name}/{channel.id}/{channel_attr}'].value = channel.attrs[channel_attr].value.rstrip()
+                        self.properties[f'{dev.name}/{channel.id}/{channel_attr}'].type = dtype
                         self.properties[f'{dev.name}/{channel.id}/{channel_attr}'].call = partial(IIO.read_iio, dev.id, path)
 
                         #attr_file_path = f'/sys/bus/iio/devices/{dev.id}/{channel.attrs[channel_attr].filename}'
@@ -204,28 +255,32 @@ class IIO:
                     pass  # not useful for us
 
                 elif device_attr.endswith('_available'):
-                    if f'{dev.name}/{channel.id}/{device_attr[:-10]}' not in self.properties:
-                        self.properties[f'{dev.name}/{channel.id}/{device_attr[:-10]}'] = EntityProperty(
-
+                    base = device_attr[:-10]
+                    dtype, adesc = ATTRIBUTE_INFO.get(base, (DataType.FLOAT, base))
+                    if f'{dev.name}/{channel.id}/{base}' not in self.properties:
+                        self.properties[f'{dev.name}/{channel.id}/{base}'] = EntityProperty(
                             category='sensor/' + dev.name,
-                            name=device_attr[:-10],
-                            description=dev.name + ' ' + channel.id + ' ' + device_attr[:-10],
-                            type=DataType.UNDEFINED,
+                            name=base,
+                            description=f"{dev.name} {channel.id} {adesc}",
+                            type=dtype,
                             interval=-1)
 
-                    self.properties[f'{dev.name}/{channel.id}/{device_attr[:-10]}'].available = dev.attrs[device_attr].value.split()
+                    self.properties[f'{dev.name}/{channel.id}/{base}'].available = dev.attrs[device_attr].value.split()
 
                 else:
                     if f'{dev.name}/{channel.id}/{device_attr}' not in self.properties:
+                        dtype, adesc = ATTRIBUTE_INFO.get(device_attr, (DataType.FLOAT, device_attr))
                         self.properties[f'{dev.name}/{channel.id}/{device_attr}'] = EntityProperty(
-
                             category='sensor/' + dev.name,
                             name=device_attr,
-                            description=dev.name + ' ' + channel.id + ' ' + device_attr,
-                            type=DataType.UNDEFINED,
+                            description=f"{dev.name} {channel.id} {adesc}",
+                            type=dtype,
                             interval=-1)
 
+                    dtype, adesc = ATTRIBUTE_INFO.get(device_attr, (DataType.FLOAT, device_attr))
                     self.properties[f'{dev.name}/{channel.id}/{device_attr}'].value = dev.attrs[device_attr].value.rstrip()
+                    self.properties[f'{dev.name}/{channel.id}/{device_attr}'].description = f"{dev.name} {channel.id} {adesc}"
+                    self.properties[f'{dev.name}/{channel.id}/{device_attr}'].type = dtype
                     self.properties[f'{dev.name}/{channel.id}/{device_attr}'].call = partial(IIO.read_iio, dev.id, dev.attrs[device_attr].filename)
 
 
